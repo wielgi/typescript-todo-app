953851555c3ac2db7397a0fb8d266f96f5a5b303
Robert Wielgusiak <wielgi88@gmail.com>
2024-04-10 21:43:18 +0200

    Base layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className}`}>
+        <main className="bg-[#1b1b3a] rounded-3xl max-w-xl mx-auto my-4">
+          <header className="flex bg-gradient-to-r from-purple-600 to-purple-500 min-h-20 rounded-t-3xl p-4 items-center shadow-lg">
+            <nav className="flex w-full justify-between">
+              <div className="burger-menu basis-2/4 text-left self-center">
+                Burger Menu
+              </div>
+              <div className="logo basis-full text-center">
+                <div className="text-center text-2xl font-medium">N-GEN</div>
+                <div className="text-center tracking-[.6rem] pl-2">TODO</div>
+              </div>
+              <div className="profile-icon basis-2/4 text-right self-center">
+                Avatar
+              </div>
+            </nav>
+          </header>
+          <section>{children}</section>
+        </main>
+      </body>
     </html>
   );
 }
